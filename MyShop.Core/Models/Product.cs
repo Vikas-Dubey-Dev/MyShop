0457daadf18fc23ec22c68229fb08038a0c4dc8f
vikas dubey <vkdubey987@hotmail.com>
2022-04-24 23:57:04 +0530

    Product Model and memory caching

---
+++ MyShop.Core/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        public string ID { get; set; }
+        
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Range(1,1000)]
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+
+
+        //ID auto Increment
+        public Product()
+        {
+            this.ID = Guid.NewGuid().ToString();
+
+        }
+    }
+}
